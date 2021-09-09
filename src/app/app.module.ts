154562e0b49da4770f34b713208e27ae5cd72d86
Dariusz <chingachgook64@gmail.com>
2021-09-09 22:47:35 +0200

    Genereted navabr, footer, banner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BannerComponent } from './banner/banner.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BannerComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
