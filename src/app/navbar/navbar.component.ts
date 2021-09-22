1b8537b7b7fb14f3ef2b15d6ca783d40fabe8cd2
Dariusz <chingachgook64@gmail.com>
2021-09-22 14:19:44 +0200

    Added navigation functions

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,3 +1,4 @@
+import { ViewportScroller } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private scroller: ViewportScroller) { }
 
   ngOnInit(): void {
+
+  }
+
+  scrollToElement(elementId: string): void {
+    this.scroller.scrollToAnchor(elementId);
   }
 
 }
