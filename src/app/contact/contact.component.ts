fa5d41862ea673948a1ce94c25e9d506b05bef20
Dariusz <chingachgook64@gmail.com>
2021-10-01 11:40:37 +0200

    Added more contnet

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -12,6 +12,8 @@ export class ContactComponent implements OnInit {
   apiLoaded: Observable<boolean>;
 
   constructor(httpClient: HttpClient) {
+
+
     this.apiLoaded = httpClient.jsonp('https://maps.googleapis.com/maps/api/js?key=AIzaSyDtAk9N9lwxNv5MSQZ-JFUlG-JXNSkZwlg', 'callback')
       .pipe(
         map(() => true),
@@ -21,5 +23,10 @@ export class ContactComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  options: google.maps.MapOptions = {
+    center: { lat: 50.018893616228745, lng: 19.952497793538118 },
+    zoom: 12,
+  };
 
 }
